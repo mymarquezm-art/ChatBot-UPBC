c3a33b019b770fe74b442cead26559c87026a703
mymarquezm-art <mymarquezm@upbc.edu.mx>
2026-02-26 12:25:46 -0800

    Update streamlit_app.py
    Faltaba try:
        __import__('pysqlite3')
        sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
    except (ImportError, KeyError):
        # Esto evita que la app truene si ya está en un sistema con sqlite actualizado
        pass

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except (ImportError, KeyError):
+    # Esto evita que la app truene si ya está en un sistema con sqlite actualizado
+    pass
 import os
 import streamlit as st
 import shutil
